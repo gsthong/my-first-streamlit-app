94b99cdbf6690c3e3c9bd303c508b5e556fd017e
dorime <73388076+gsthong@users.noreply.github.com>
2023-03-16 17:11:35 +0700

    Update predicttemp.py

--- predicttemp.py
+++ predicttemp.py
@@ -9,16 +9,13 @@ drive.mount('/content/drive')
 df = pd.read_csv('/content/drive/MyDrive/IceCreamData.csv')
 df.head()
 
-x = df['Temperature'].values
-y = df['Revenue'].values
+x = df['Temperature'].values.reshape(-1, 1)
+y = df['Revenue'].values.reshape(-1, 1)
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 50 , random_state = 43)
-x_train = np.array((x_train))
-x_test = np.array((x_test))
-y_train = np.array((y_train))
-y_test = np.array((y_test))
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=50, random_state=43)
 
-y_line = model.predict(x)
+model = LinearRegression()
+model.fit(x_train, y_train)
 
-f = open('model.pickle', 'wb')
-pickle.dump(model, f)
+with open('model.pickle', 'wb') as f:
+    pickle.dump(model, f)
