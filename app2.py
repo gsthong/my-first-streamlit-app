355f4dbe5167ef6e7d5f6d49dd8d57b998c228f6
dorime <73388076+gsthong@users.noreply.github.com>
2023-03-17 16:02:01 +0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -2,13 +2,11 @@ import streamlit as st
 import pickle
 import numpy as np
 
-st.title("Ice Cream Sales Prediction")
+st.title("Revenue Prediction")
 
 f = open('model.pickle', 'rb')
 model = pickle.load(f)
 
-# x_new= (np.array(st.number_input("Input Temperature"))).reshape(-1,1)
-# y_new = model.predict(x_new)
 temperature = st.number_input("Enter temperature (in Celsius)")
 y_new = model.predict(np.array(temperature).reshape(-1, 1))
 
